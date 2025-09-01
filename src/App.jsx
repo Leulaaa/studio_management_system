4d3047f2a159f418f9dfa04eb2f3a4f0f070c393
Leul <leulaaa12@gmail.com>
2025-09-01 14:12:23 +0300

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import { Dashboard } from './page/dashboard'
 import {Sign1} from './page/signup'
 import { Login1 } from './page/login'
--- src/page/dashboard.jsx
+++ src/page/dashboard.jsx
@@ -1,16 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import './dashboard.css';
 import { ImStack } from 'react-icons/im'
-
-
-
+import { RiStore2Line } from 'react-icons/ri'
+import { FiSettings } from 'react-icons/fi'
+import { FiLogOut } from 'react-icons/fi'
 
 export function Dashboard() {
   return (
-  
-    <div className="dashboard"id='dashboard'>
+  <>
+   <body className="dashboard"id='dashboard'>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    </body>
+    <div id='dashboard'>
+      <ul id='nav'> 
+        <li id='active'><Link className='h' id='h3' to="/dashboard"><ImStack /> <h2 id='h2'>Dashboard</h2></Link></li>
+        <li><Link id='h' to="/addstudent"> <RiStore2Line  /> <h2 id='h2'>Booking History</h2></Link></li>
+        <li><Link id='h' to="/addcourse"><FiSettings /> <h2 id='h2'>Setting</h2></Link></li>
+        <li><Link id='h' to="/addcourse"><FiLogOut /><h2 id='h2'>Log-out</h2></Link></li>
+      </ul>
+      <div>
+        <h1 id='welcome'>Hello, Abebe</h1>
+        <h3 id='welcome1'>From your account dashboard. you can easily check & view your <br /> 
+        Recent Bookings, manage your payments and edit your <br /> 
+        Password and Account Details. </h3>
+      </div>
       
-      return <h3> Lets go for a <ImStack />? </h3>
+
     </div>
-  
+  </>
   )
 }
